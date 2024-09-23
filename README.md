a6073572be425195b489beb62b7244888b6950d2
YessiMG <malaveryess@gmail.com>
2024-09-23 16:03:16 -0500

    YM Solution to the challenge

---
+++ README.md
@@ -0,0 +1,7 @@
+# Inmersión Dev Clase 01 - Generador de contraseñas 
+Durante la clase se explicó un código inicial, que corresponde al primer commit de este repositorio. Al finalizar se planteó el siguiente desafio:
+- Agregar símbolos especiales cómo !@#$%^&*() a la cadena de caracteres para que se creen contraseñas más seguras.
+- Limpiar el campo de la contraseña, agregando un nuevo botón de limpiar.
+- Implementar una validación que avise al usuario si la contraseña generada es fuerte o débil (por ejemplo, si no contiene un número, o una letra mayúscula) y que muestre un mensaje donde informe que la contraseña es débil.
+
+`#InmersionDevAlura`
--- main.js
+++ main.js
@@ -2,22 +2,55 @@ let count = document.getElementById('cantidad');
 let buttonAction = document.getElementById('generar');
 let passwordField = document.getElementById('contrasena');
 
-const characterString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const characterString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
 function generate() {
     let num = parseInt(count.value);
 
     if (num < 8) {
-        alert("La cantidad de caracteres tiene que ser mayor que 8");
+        cleanPassword();
+
+        setTimeout(() => {
+            alert("La cantidad de caracteres tiene que ser mayor que 8");
+        }, 0); 
     }
+    else {
+        let password = '';
+    
+        for(let i = 0; i < num; i++) {
+            let randomCharacter = characterString[Math.floor(Math.random() * characterString.length)];
+    
+            password += randomCharacter;
+        }
+    
+        passwordField.value = password;
 
-    let password = '';
-
-    for(let i = 0; i < num; i++) {
-        let randomCharacter = characterString[Math.floor(Math.random() * characterString.length)];
-
-        password += randomCharacter;
+        setTimeout(() => {
+            warnPasswordStrength(password);
+        }, 0); 
     }
+}
 
-    passwordField.value = password;
+function cleanPassword() {
+    passwordField.value = '';
+}
+
+function warnPasswordStrength(password) {
+    let hasUpperCase = /[A-Z]/.test(password);
+    let hasLetter = /[a-zA-Z]/.test(password);
+    let hasNumber = /[0-9]/.test(password);
+    let hasSpecialCharacter = /[!@#$%^&*()]/.test(password);
+
+    if (hasUpperCase && hasLetter && hasNumber && hasSpecialCharacter) {
+        alert("La contraseña es fuerte, cumple con tener una letra mayúscula y minúscula, un número y un caractér especial");
+    }
+    else {
+        let warningMessage = `La contraseña es débil, le hace falta tener:\n`;
+        warningMessage += !hasUpperCase ? "- una letra mayúscula\n" : '';
+        warningMessage += !hasLetter ? "- una letra minúscula\n" : '';
+        warningMessage += !hasNumber ? "- un número\n" : '';
+        warningMessage += !hasSpecialCharacter ? "- un carácter especial\n" : '';
+
+        alert(warningMessage.trim());
+    }
 }
